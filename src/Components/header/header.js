49a63d4185d306b01bbf381455be3ff53eada0a5
adunham95 <adunham95@gmail.com>
2017-06-02 13:35:40 -0400

    Initial commit

---
+++ src/Components/header/header.js
@@ -0,0 +1,17 @@
+/**
+ * Created by apdun on 6/2/2017.
+ */
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Header;
