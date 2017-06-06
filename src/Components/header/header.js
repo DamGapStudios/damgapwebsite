a68cebdccddd7d50e5ec106ec38c2f234f6cb306
adunham95 <adunham95@gmail.com>
2017-06-05 21:38:06 -0400

    Add blg page

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -2,22 +2,54 @@
  * Created by apdun on 6/2/2017.
  */
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './header.css'
 
-class Header extends Component {
+export default class Header extends Component {
+
 
     render() {
         return (
             <div>
-                DamGap Studios
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                </ul>
+                <nav className="nav">
+                    <div className="nav-left">
+                        <a className="nav-item">
+                           DamGap Studios
+                        </a>
+                    </div>
+
+                    <div className="nav-center">
+                        <a className="nav-item">
+                          <span className="icon">
+                            <i className="fa fa-github"></i>
+                          </span>
+                        </a>
+                        <a className="nav-item">
+                          <span className="icon">
+                            <i className="fa fa-twitter"></i>
+                          </span>
+                        </a>
+                    </div>
+
+                    {/*<!-- This "nav-toggle" hamburger menu is only visible on mobile -->*/}
+                    {/*<!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->*/}
+                    <span className="nav-toggle">
+                        <Link to="/" className="nav-item">Home</Link>
+                        <Link to="/about" className="nav-item">About</Link>
+                        <Link to="/projects" className="nav-item">Projects</Link>
+                        <Link to="/blog" className="nav-item">Blog</Link>
+                    </span>
+
+                    {/*<!-- This "nav-menu" is hidden on mobile -->*/}
+                    {/*<!-- Add the modifier "is-active" to display it on mobile -->*/}
+                    <div className="nav-right nav-menu">
+                        <Link to="/" className="nav-item">Home</Link>
+                        <Link to="/about" className="nav-item">About</Link>
+                        <Link to="/projects" className="nav-item">Projects</Link>
+                        <Link to="/blog" className="nav-item">Blog</Link>
+                    </div>
+                </nav>
             </div>
         );
     }
 }
-
-export default Header;
---
+++ src/Views/Blog/Blog.js
@@ -0,0 +1,17 @@
+/**
+ * Created by apdun on 6/5/2017.
+ */
+import React, { Component } from 'react';
+
+class Blog extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Blog</h2>
+            </div>
+        );
+    }
+}
+
+export default Blog;
