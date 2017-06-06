35f3d9d9b0ca83d95a3477c81070824dd5564731
adunham95 <adunham95@gmail.com>
2017-06-06 13:24:19 -0400

    Progress on nice looking projects page

---
+++ src/Components/projectBlock/projectBlock.js
@@ -0,0 +1,32 @@
+/**
+ * Created by apdun on 6/6/2017.
+ */
+import React, { Component } from 'react';
+import './projectBlock.css'
+
+export default class ProjectBlock extends Component {
+
+    render() {
+        return (
+            <div>
+                <section className="hero" style={{backgroundColor: this.props.color}}>
+                    <div className="hero-body">
+                        <div className="container">
+                            <img src={this.props.image} alt="" className="heroImage" style={{boxShadow: '10px 10px 20px' + this.props.accent}}/>
+
+                            <div className="innerContainer">
+                                <h1 className="title" style={{color: this.props.accent}}>
+                                    { this.props.name }
+                                </h1>
+                                <h2 className="subtitle" style={{color: this.props.accent}}>
+                                    { this.props.description }
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        );
+    }
+}
--- src/Views/Projects/Projects.js
+++ src/Views/Projects/Projects.js
@@ -2,16 +2,15 @@
  * Created by apdun on 6/5/2017.
  */
 import React, { Component } from 'react';
+import ProjectBlock from "../../Components/projectBlock/projectBlock";
 
 class Projects extends Component {
 
     render() {
         return (
             <div>
-                <h2>Projects</h2>
-                <ul>
-                    <li>Project 1</li>
-                </ul>
+                <ProjectBlock name="AshleyTLee.com" description="Some Test here" color="#8AD0D5" accent="#4B7FC0" image="AshleyTLee.PNG"/>
+                <ProjectBlock name="TravelJuliasWay.com" description="Some Test here" color="#3C4768" accent="#EBECEC"  image="TravelJuliasWay.PNG"/>
             </div>
         );
     }
