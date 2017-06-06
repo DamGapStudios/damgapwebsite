ed354b01cb6ea7643ebce8194d2a7e01a1e065ed
adunham95 <adunham95@gmail.com>
2017-06-05 22:35:30 -0400

    Added christmas hero SANTA

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -2,13 +2,42 @@
  * Created by apdun on 6/5/2017.
  */
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
 
     render() {
         return (
             <div>
-                <h2>Home</h2>
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Develop
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Create
+                            </h1>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="hero is-primary">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Impress
+                            </h1>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
