d1f5d97254733f19075bf71362eaaf8dccd24e3b
adunham95 <Apd@21295>
2017-06-19 12:08:39 -0400

    Added about us page

--- src/Views/About/About.js
+++ src/Views/About/About.js
@@ -11,34 +11,65 @@ class About extends Component {
     render() {
         return (
             <div>
-                <Hero>
-                    <HeroBody className="hero is-primary" isSize="medium">
-                        <Container hasTextAlign='centered'>
-                            <Title>About Us</Title>
-                        </Container>
-                    </HeroBody>
-                </Hero>
+                {/*<Hero>*/}
+                    {/*<HeroBody className="hero is-primary" isSize="medium">*/}
+                        {/*<Container hasTextAlign='centered'>*/}
+                            {/*<Title>About Us</Title>*/}
+                        {/*</Container>*/}
+                    {/*</HeroBody>*/}
+                {/*</Hero>*/}
 
                 <Section className="mission">
                     <Content>
                         <Columns isCentered>
-                            {/*<Column isMobile={0} isDesktop={3}>Words</Column>*/}
-                            <Column isMobile={12} isDesktop={6}>
-                                <Box className="mission">
-                                    <div>
-                                        <FontAwesome name='users' size='2x'/>
-                                        <h1 className="mission title">Our Mission</h1>
-                                        <p>To create magnificent websites using the latest in web technologies to create a unique user experience</p>
-                                    </div>
-                                </Box>
-                            </Column>
-                            {/*<Column isMobile={0} isDesktop={3}>Words</Column>*/}
+                            <div>
+                                <FontAwesome name='fire' size='2x'/>
+                                <h1>Our Mission</h1>
+                            </div>
                         </Columns>
-
-                        <Columns>
-                            <Column>
-
+                        <Columns isCentered>
+                            <Column isMobile={12} isDesktop={6}>
+                                <p>To create magnificent websites using the latest in web technologies to create a unique user experience</p>
                             </Column>
+
+                            <Column isMobile={12} isDesktop={6}>
+                                <p>To create magnificent websites using the latest in web technologies to create a unique user experience</p>
+                            </Column>
+                        </Columns>
+                    </Content>
+                </Section>
+                <Section className="whoWeAre">
+                    <Content>
+                        <Columns isCentered>
+                            <div>
+                                <FontAwesome name='group' size='2x' className="whoWeAre"/>
+                                <h1 className="whoWeAre">Who We Are</h1>
+                                <p className="whoWeAre">
+                                    We are a group of passionate developers that are looking to push the boundaries of technology.
+                                </p>
+                            </div>
+                        </Columns>
+                        <Columns>
+                            <Column isMobile={12} isDesktop={6}>
+                                <div  className="whoWeAre profile">
+                                    <h3 className="whoWeAre titleProfile" >Adrian Dunham</h3>
+                                    <h4 className="whoWeAre subProfile" >Lead Developer</h4>
+                                    <p>
+
+                                    </p>
+                                </div>
+                            </Column>
+
+                            <Column isMobile={12} isDesktop={6}>
+                                <div  className="whoWeAre profile">
+                                    <h3 className="whoWeAre titleProfile" >Anthony Garera</h3>
+                                    <h4 className="whoWeAre subProfile" >Lead Client Liaison/Developer</h4>
+                                    <p>
+
+                                    </p>
+                                </div>
+                            </Column>
+
                         </Columns>
                     </Content>
                 </Section>
