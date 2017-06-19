2a5bc189e90d389b7c6dd7b1b42ff7b1db6ecaee
adunham95 <Apd@21295>
2017-06-19 01:31:11 -0400

    Updates homepage Redid home Added mission

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -3,41 +3,55 @@
  */
 import React, { Component } from 'react';
 import './Home.css'
+import {Hero, HeroBody, Container, Title, Section, Columns, Column, Notification, Content} from 'bloomer'
+import FontAwesome from 'react-fontawesome';
 
 class Home extends Component {
 
     render() {
         return (
             <div>
-                <section className="hero is-primary">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Develop
-                            </h1>
-                        </div>
-                    </div>
-                </section>
 
-                <section className="hero is-primary">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Create
-                            </h1>
-                        </div>
-                    </div>
-                </section>
+                <Hero className="hero is-primary" isSize='large'>
 
-                <section className="hero is-primary">
-                    <div className="hero-body">
-                        <div className="container">
-                            <h1 className="title">
-                                Impress
-                            </h1>
-                        </div>
-                    </div>
-                </section>
+                    <HeroBody>
+                        <Container hasTextAlign='centered'>
+                            {/*Slider Here*/}
+                            <Title><span className="text-develop">Develop</span> | <span className="text-create">Create</span> | <span className="text-impress">Impress</span></Title>
+                        </Container>
+                    </HeroBody>
+
+                </Hero>
+
+
+                <Section>
+                    <Columns>
+                        <Column isMobile={12} isDesktop={5}>
+                            <Notification hasTextAlign='centered' className="wwa"> <Title isSize={2}>What we do</Title></Notification>
+                        </Column>
+
+                        <Column isMobile={12} isDesktop={7}>
+                            <Content>
+                                <div className="wwa">
+                                    <h1 className="wwa title"><FontAwesome name='code' /> Develop</h1>
+                                    <p className="wwa content">Developing site to help you </p>
+                                </div>
+
+                                <div className="wwa">
+                                    <h1 className="wwa title"><FontAwesome name='paint-brush' /> Create</h1>
+                                    <p className="wwa content">Creating masterpieces that push technology </p>
+                                </div>
+
+                                <div className="wwa">
+                                    <h1 className="wwa title"><FontAwesome name='handshake-o' /> Impress</h1>
+                                    <p className="wwa content">Show off you site with Pride! </p>
+                                </div>
+                            </Content>
+                        </Column>
+                    </Columns>
+                </Section>
+
+                {/*Work With Element Here*/}
 
             </div>
         );
